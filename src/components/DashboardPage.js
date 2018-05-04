febd04449124196c3564e8ae9e3c5578556bc649
Petru <escu.petru@gmail.com>
2018-05-04 12:37:47 +0300

    first commit

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Appbar, Button, Panel } from 'muicss/react';
+
 
 const DashboardPage = () => (
   <div>
     Dashboard page content
+    <Appbar />
+          <Panel>
+            <Button className="button">My Button</Button>
+          </Panel>
+
   </div>
 );
 
